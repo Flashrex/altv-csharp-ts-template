1d1e6aac26d547a6900b55f554c473625e9e6d59
DominikDuda <Dominik.Duda96@gmx.de>
2024-05-31 02:30:21 +0200

    update for dotnet ef database command usage

--- server/Database/DatabaseContext.cs
+++ server/Database/DatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Server.Database.Models;
 
 namespace Server.Database {
@@ -8,7 +9,19 @@ namespace Server.Database {
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
 
-            var conString = Core.Server.GetConfiguration()?["ConnectionString"];
+            var config = Core.Server.GetConfiguration();
+
+            if (config == null) {
+                var basePath = Directory.GetCurrentDirectory();
+
+                var builder = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile(basePath + "/appsettings.json", optional: false, reloadOnChange: true);
+
+                config = builder.Build();
+            }
+
+            var conString = config["ConnectionString"];
             optionsBuilder.UseMySql(conString, ServerVersion.AutoDetect(conString));
         }
 
